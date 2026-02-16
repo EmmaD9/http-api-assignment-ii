6651e4664c73c1692a0f38cb870f63f1d2ce154d
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2026-02-15 21:52:29 -0500

    responses progress

--- server/responses.js
+++ server/responses.js
@@ -1,31 +1,77 @@
-const handleGet = (pathname, req, res) => {
-    if(pathname = '/getUsers') return getUsersGET(req, res);
-    if (pathname = '/notReal') return notRealGET(req, res);
-    if (pathname = '/') return htmlResponses.getIndex(req, res);
-    if (pathname = '/style.css') return htmlResponses.getCss(req, res);
+const handleGET = (pathname, request, response) => {
+    if(pathname = '/getUsers') return getUsersGET(request, response);
+    if (pathname = '/notReal') return notRealGET(request, response);
+    if (pathname = '/') return htmlResponses.getIndex(request, response);
+    if (pathname = '/style.css') return htmlResponses.getCss(request, response);
 
-    return notFoundGET(req, res);
-}
+    return notFoundGET(request, response);
+};
 
-const handleHEAD = (pathname, req, res) => {
-    if(pathname ===  '/getUsers') return getUsersHEAD(req, res);
-    if (pathname === '/notReal') return notRealHEAD(req, res);
+const handleHEAD = (pathname, request, response) => {
+    if(pathname ===  '/getUsers') return getUsersHEAD(request, response);
+    if (pathname === '/notReal') return notRealHEAD(request, response);
 
-    return notFoundHEAD(req, res);
-}
+    return notFoundHEAD(request, response);
+};
 
-const handlePOST = (pathname, req, res) => {
-    if(pathname === '/addUser') return addUserPOST(req, res);
+const handlePOST = (pathname, request, response) => {
+    if(pathname === '/addUser') return addUserPOST(request, response);
 
-    return notFoundGET(req, res);
-}
+    return notFoundGET(request, response);
+};
 
-//get users json and no body
+//user storage:
+const users = {};
 
-//get not real
+//GET users JSON
+const getUsersGET = (request, response) => {
+    const responseJSON = { users };
+
+    response.writeHead(200, {'Content-Type' : 'application/json'});
+    response.write(JSON.stringify(responseJSON));
+    response.end();
+};
+
+//GET users JSON but with no body
+const getUsersHEAD = (request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json'});
+    response.end();
+};
+
+//get not real for JSON error
+const notRealGET = (request, response) => {
+    const responseJSON = {
+        message: 'The page you are looking for was not found',
+        id: 'notFound',   
+    };
+
+    response.writeHead(404, {'Content-Type': 'application/json'});
+    response.write(JSON.stringify(responseJSON));
+    response.end();
+};
 
 //head not real
+const notRealHEAD = (req, res) =>{
+    response.writeHead(404, {'Content-Type': 'application/json'});
+    response.end();
+}
 
-//post add user
+//TODO: post add user
 
-//404 error page
+//GET 404 error page with JSON
+const notFoundGET = (request, response) => {
+    const responseJSON = {
+        message: 'The page you are looking for was not found',
+        id: 'notFound',  
+    }
+
+    response.writeHead(404, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(responseJSON));
+    response.end();
+}
+
+//HEAD 404 version with no body
+const notFoundHead = (request, response) => {
+    response.writeHead(404, {'Content-Type': 'application/json' });
+    response.end();
+};

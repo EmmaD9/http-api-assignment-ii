9f638c47aaec97b50816c7359fe5759d4dabc084
EmmaD9 <99094133+EmmaD9@users.noreply.github.com>
2026-02-15 21:14:01 -0500

    routing should work?

--- server/responses.js
+++ server/responses.js
@@ -3,4 +3,19 @@ const handleGet = (pathname, req, res) => {
     if (pathname = '/notReal') return notRealGET(req, res);
     if (pathname = '/') return htmlResponses.getIndex(req, res);
     if (pathname = '/style.css') return htmlResponses.getCss(req, res);
+
+    return notFoundGET(req, res);
+}
+
+const handleHEAD = (pathname, req, res) => {
+    if(pathname ===  '/getUsers') return getUsersHEAD(req, res);
+    if (pathname === '/notReal') return notRealHEAD(req, res);
+
+    return notFoundHEAD(req, res);
+}
+
+const handlePOST = (pathname, req, res) => {
+    if(pathname === '/addUser') return addUserPOST(req, res);
+
+    return notFoundGET(req, res);
 }
